aafeb99304d6e9f2cce7a0dc540a994ccf7c9975
ishiikurisu <ishiikurisu@yahoo.com>
2015-12-03 21:41:44 -0200

    Adding Registration Page

--- WhatToDo/WhatToDo/View/PaginaRegistrar.xaml.cs
+++ WhatToDo/WhatToDo/View/PaginaRegistrar.xaml.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using WhatToDo.View;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -14,30 +12,39 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace WhatToDo
+namespace WhatToDo.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class IntroPage: Page
+    public sealed partial class PaginaRegistrar : Page
     {
-        public IntroPage()
+        public PaginaRegistrar()
         {
             this.InitializeComponent();
         }
 
-        private void ButtonRegistrar_Click(object sender, RoutedEventArgs e)
+        private void ButtonFinalizar_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(PaginaRegistrar));
-        }
+            /* turn "Collapsed" to "Visible" */
+            string nome = TextNome.Text;
+            string email = TextEmail.Text;
+            string senha = PasswordSenha.Password;
 
-        private async void ButtonEntrar_Click(object sender, RoutedEventArgs e)
-        {
-            MessageDialog msg = new MessageDialog("Entre!");
-            await msg.ShowAsync();
+            //var msg = new MessageDialog(string.Format("{0} uses {1} as a password. What a faggot.", nome, senha));
+            //await msg.ShowAsync();
+            if ()
+            {
+
+            }
+            else
+            {
+                LabelErro.
+            }
         }
     }
 }

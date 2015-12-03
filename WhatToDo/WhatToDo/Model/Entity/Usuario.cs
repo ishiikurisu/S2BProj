76ee80043f99d8dfd8f6c5715a9eb61bb0a99376
ygaum <ygordanniel@gmail.com>
2015-12-03 21:40:12 -0200

    Usuario and BancoDeDados class implementation.

--- WhatToDo/WhatToDo/Model/Entity/Usuario.cs
+++ WhatToDo/WhatToDo/Model/Entity/Usuario.cs
@@ -12,5 +12,12 @@ namespace WhatToDo.Model.Entity
         public string Email { get; set; }
         public string Senha { get; set; }
         public string Nome { get; set; }
+
+        //Temporary implementation - Validate if Usuario already exists
+        public override bool Equals(object obj)
+        {
+            Usuario elem = (Usuario)obj;
+            return elem.Email.ToUpper().Equals(Email.ToUpper());
+        }
     }
 }

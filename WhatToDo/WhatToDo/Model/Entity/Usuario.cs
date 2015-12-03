d89151dfc28b5904c515997839704224a3ccf19b
ygaum <ygordanniel@gmail.com>
2015-12-03 21:24:16 -0200

    New propeties on Usuario class

--- WhatToDo/WhatToDo/Model/Entity/Usuario.cs
+++ WhatToDo/WhatToDo/Model/Entity/Usuario.cs
@@ -9,6 +9,8 @@ namespace WhatToDo.Model.Entity
     class Usuario
     {
         public int IdUsuario { get; set; }
-
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
     }
 }

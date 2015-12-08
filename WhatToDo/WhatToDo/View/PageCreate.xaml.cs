ab935bd84fddad4406fa524f9f5c06614adbbd09
ygaum <ygordanniel@gmail.com>
2015-12-07 22:29:41 -0200

    Stupid Git!

--- WhatToDo/WhatToDo/View/PageCreate.xaml.cs
+++ WhatToDo/WhatToDo/View/PageCreate.xaml.cs
@@ -80,17 +80,5 @@ namespace WhatToDo.View
 
             MenuOpened = !MenuOpened;
         }
-
-
-        async private void MyMap_PointerPressedOverride(object sender, PointerRoutedEventArgs e)
-        {
-            Bing.Maps.Location l = new Bing.Maps.Location();
-            this.MyMap.TryPixelToLocation(e.GetCurrentPoint(this.MyMap).Position, out l);
-            Bing.Maps.Pushpin pushpin = new Bing.Maps.Pushpin();
-            pushpin.SetValue(Bing.Maps.MapLayer.PositionProperty, l);
-            var msg = new MessageDialog(e.GetCurrentPoint(MyMap).Position.ToString());
-            await msg.ShowAsync();
-            this.MyMap.Children.Add(pushpin);
-        }
     }
 }

2cb871b3b833068f62a6002f53f85f5ae454e42a
ishiikurisu <ishiikurisu@yahoo.com>
2015-12-08 21:35:29 -0200

    ...

--- WhatToDo/WhatToDo/View/PageCreate.xaml.cs
+++ WhatToDo/WhatToDo/View/PageCreate.xaml.cs
@@ -95,12 +95,12 @@ namespace WhatToDo.View
         private void AddPin(PointerRoutedEventArgs e)
         {
             Bing.Maps.Location l = new Bing.Maps.Location();
-            this.MyMap.TryPixelToLocation(e.GetCurrentPoint(this.MyMap).Position, out l);
+            MyMap.TryPixelToLocation(e.GetCurrentPoint(this.MyMap).Position, out l);
             Bing.Maps.Pushpin pushpin = new Bing.Maps.Pushpin();
             pushpin.SetValue(Bing.Maps.MapLayer.PositionProperty, l);
             TextLocal.Text = "" + l.Latitude.ToString() + " " + l.Longitude.ToString();
             MyMap.Children.Clear();
-            this.MyMap.Children.Add(pushpin);
+            MyMap.Children.Add(pushpin);
         }
 
         private void ButtonCreate_Click(object sender, RoutedEventArgs e)
